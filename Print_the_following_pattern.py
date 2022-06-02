731c73a3e79bcb5d10d7c2a37212c36db2c85bc5
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-06-02 23:23:06 +0530

    n=int(input()) s="ABCDEFGHIJKLMNOPQRSTUVWXYZ" q=0 k = n-1 for i in range(0, n):     for j in range(0, k):         print(end=" ")     k-=1     for j in range(0,i+1):         print(s[j], end="")         q+=1     for j in range(i-1,-1,-1):         print(s[j], end="")         q-=1     print()

--- Print_the_following_pattern.py
+++ Print_the_following_pattern.py
@@ -1,8 +1,15 @@
-n=int(input())#5
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if i==j or i==n+1-j:
-            print(n-i+1,end=" ")
-        else:
-            print(" ",end="")
+n=int(input())
+s="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+q=0
+k = n-1
+for i in range(0, n):
+    for j in range(0, k):
+        print(end=" ")
+    k-=1
+    for j in range(0,i+1):
+        print(s[j], end="")
+        q+=1
+    for j in range(i-1,-1,-1):
+        print(s[j], end="")
+        q-=1
     print()
