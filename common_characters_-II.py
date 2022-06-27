0485bf7bf8be3b0f4e5c562f268ffda3cadcb4e1
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-06-27 21:23:39 +0530

    s1=input() v=s1.lower() res=v.split() s2=input() u=s2.lower() h=u.split() w="" q="" e="" for i in res:     for j in i:         if j not in w:             w+=j for i in h:     for j in i:         if j not in q:             q+=j for i in w:     if i in q and i!=' ':         e+=i v=''.join(sorted(e)) print(len(e))

---
+++ common_characters_-II.py
@@ -0,0 +1,25 @@
+s1=input()
+v=s1.lower()
+res=v.split()
+s2=input()
+u=s2.lower()
+h=u.split()
+w=""
+q=""
+e=""
+for i in res:
+    for j in i:
+        if j not in w:
+            w+=j
+for i in h:
+    for j in i:
+        if j not in q:
+            q+=j
+for i in w:
+    if i in q and i!=' ':
+        e+=i
+v=''.join(sorted(e))
+print(len(e))
+
+        
+        
