1b8c26175c202b8984ce8318f492713a4c0e98de
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-06-27 21:19:39 +0530

    s1=input() v=s1.lower() res=v.split() s2=input() u=s2.lower() h=u.split() w="" q="" e="" for i in res:     for j in i:         if j not in w:             w+=j for i in h:     for j in i:         if j not in q:             q+=j for i in w:     if i not in q and i!=' ':         e+=i for i in q:     if i not in w and i!=' ':         e+=i print(len(e))

---
+++ Non_common_characters.py
@@ -0,0 +1,27 @@
+s1=input()
+v=s1.lower()
+res=v.split()
+s2=input()
+u=s2.lower()
+h=u.split()
+w=""
+q=""
+e=""
+for i in res:
+    for j in i:
+        if j not in w:
+            w+=j
+for i in h:
+    for j in i:
+        if j not in q:
+            q+=j
+for i in w:
+    if i not in q and i!=' ':
+        e+=i
+for i in q:
+    if i not in w and i!=' ':
+        e+=i
+print(len(e))
+
+        
+        
