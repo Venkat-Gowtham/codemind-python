587d1864922e0c1ed10e79a8b0bd58cd7d3b1f5c
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-06-29 15:19:19 +0530

    n=int(input()) a=list(map(int,input().split())) i=0 while i<n-1:     j=i     while a[j+1]:         print(a[i],end=" ")         a[j+1]-=1     i+=2

--- Generate_array_II.py
+++ Generate_array_II.py
@@ -1,10 +1,9 @@
 n=int(input())
 a=list(map(int,input().split()))
-c=0
 i=0
 while i<n-1:
     j=i
     while a[j+1]:
-        print(a[j],end=" ")
+        print(a[i],end=" ")
         a[j+1]-=1
     i+=2
