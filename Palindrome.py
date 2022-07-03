cfb282deb19f519e6b2757b9f8a38b2c26c00573
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-07-03 21:04:55 +0530

    n=int(input()) q=str(n) q=q[::-1] if int(q)==n:     print(True) else:     print(False)

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,7 @@
 n=int(input())
-v=n
-rev=0
-while(n!=0) :
-    r=n%10
-    rev=rev*10+r
-    n=n//10
-if rev==v :
-    print('True')
-else :
-    print('False')
+q=str(n)
+q=q[::-1]
+if int(q)==n:
+    print(True)
+else:
+    print(False)
