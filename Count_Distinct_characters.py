5ec1d5c74f95ec5693fdba0b7aaf1a62f96e4327
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-07-04 21:12:02 +0530

    n=input() n=n.lower() c=0 q="" for i in n:     if i not in q and i!=' ':         q+=i         c+=1 print(len(q))

--- Count_Distinct_characters.py
+++ Count_Distinct_characters.py
@@ -1,13 +1,10 @@
-s=input()
-x=len(s)
-c=v=z=w=0
-b=[]
-for i in range(x):
-    c=0
-    if s[i]==" ":
-        continue
-    else:
-        if s[i].upper() not in b and s[i].lower() not in b:
-            b.append(s[i])
-v=len(b)
-print(v)
+n=input()
+n=n.lower()
+c=0
+q=""
+for i in n:
+    if i not in q and i!=' ':
+        q+=i
+        c+=1
+print(len(q))
+    
