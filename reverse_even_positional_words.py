26f2698d43c13f55215b54c68ce78f0bde8b22fc
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-07-04 23:20:48 +0530

    n=input() n=n.split() c=0 x=0 q="" for i in n:     c+=1 #print(c) for i in n:     if x<c:         if x%2==0:             q+=i[::-1]         else:             q+=i     if x+1<c:         q+=' '     x+=1 print(q)

--- reverse_even_positional_words.py
+++ reverse_even_positional_words.py
@@ -1,11 +1,18 @@
 n=input()
-z=n.split()
+n=n.split()
 c=0
-for i in z:
-    if c%2==0:
-        print(i[::-1],end=" ")
-    else:
-        print(i,end=" ")
+x=0
+q=""
+for i in n:
     c+=1
-       
-    
+#print(c)
+for i in n:
+    if x<c:
+        if x%2==0:
+            q+=i[::-1]
+        else:
+            q+=i
+    if x+1<c:
+        q+=' '
+    x+=1
+print(q)        
