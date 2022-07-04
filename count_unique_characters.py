e47312ed83a67ba373a551ba9cbe1390f4cc4974
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-07-04 21:16:35 +0530

    n=input() n=n.lower() c=0 q="" for i in n:     if n.count(i)==1 and i!=' ':         c+=1 print(c)

--- count_unique_characters.py
+++ count_unique_characters.py
@@ -1,17 +1,8 @@
-s=input()
-x=len(s)
-c=v=u=0
-for i in range(x):
-    c=0
-    if s[i]!=" ":
-        for j in range(x):
-            if i!=j:
-                if s[i]==s[j] or s[i].upper()==s[j] or s[i].lower()==s[j]:
-                    c+=1
-        if(c==0):
-            u+=1
-print(u)
-
-
-    
-    
+n=input()
+n=n.lower()
+c=0
+q=""
+for i in n:
+    if n.count(i)==1 and i!=' ':
+        c+=1
+print(c)
