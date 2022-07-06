8a3bd7a3abacb3850a7f097d806efbdd3cf7dce6
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-07-06 20:55:59 +0530

    n=input() n=n.lower() s=input() s=s.lower() q="" for i in n:     if i not in s and i not in q and i!=' ':         q+=i for i in s:     if i not in n and i not in q and i!=' ':         q+=i x="".join(sorted(q)) print(x)

--- non_common_characters.py
+++ non_common_characters.py
@@ -1,28 +1,13 @@
-s1=input()
-v=s1.lower()
-res=v.split()
-s2=input()
-u=s2.lower()
-h=u.split()
-w=""
+n=input()
+n=n.lower()
+s=input()
+s=s.lower()
 q=""
-e=""
-for i in res:
-    for j in i:
-        if j not in w:
-            w+=j
-for i in h:
-    for j in i:
-        if j not in q:
-            q+=j
-for i in w:
-    if i not in q and i!=' ':
-        e+=i
-for i in q:
-    if i not in w and i!=' ':
-        e+=i
-v=''.join(sorted(e))
-print(v)
-
-        
-        
+for i in n:
+    if i not in s and i not in q and i!=' ':
+        q+=i
+for i in s:
+    if i not in n and i not in q and i!=' ':
+        q+=i
+x="".join(sorted(q))
+print(x)
