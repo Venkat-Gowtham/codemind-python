ad68bb4caf206d5967383d5fc7560ad113136a73
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-07-20 15:53:27 +0530

    s1=input().lower() s1=s1.split() r=s1[0] res=[] for i in r:     for j in s1:         if i not in j:             break     else:         res.append(i) if(len(res)>0):     res.sort()     res=''.join(res)     print(res[0]) else:     print(-1)

--- Common_characters_-_3.py
+++ Common_characters_-_3.py
@@ -1,17 +1,16 @@
-s=input()
-s=s.lower()
-s=list(s.split())
-f=s[0]
+s1=input().lower()
+s1=s1.split()
+r=s1[0]
 res=[]
-for i in f:
-    for j in s:
+for i in r:
+    for j in s1:
         if i not in j:
             break
     else:
         res.append(i)
-res.sort()
-res=''.join(res)
-if len(res)>0:
+if(len(res)>0):
+    res.sort()
+    res=''.join(res)
     print(res[0])
 else:
     print(-1)
