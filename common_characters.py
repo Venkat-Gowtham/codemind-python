c3128ed8a412cd3578f0a1fcb52a81a9e4016b14
Venkat-Gowtham <97425763+Venkat-Gowtham@users.noreply.github.com>
2022-07-20 15:38:50 +0530

    s1=input().lower() s2=input().lower() res="" for i in s1:     if i in s2 and i not in res and i!=' ':         res+=i res=list(res) res.sort() res=''.join(res) if len(res)!=0:     print(res) else:     print(-1)

--- common_characters.py
+++ common_characters.py
@@ -1,16 +1,13 @@
 s1=input().lower()
 s2=input().lower()
-s1=list(s1)
-s2=list(s2)
-res=[]
+res=""
 for i in s1:
-    if i in s2 :
-        if i not in res:
-            res.append(i)
-#print(res)
-res=''.join(sorted(res))
-res=res.strip()
-if(len(res)>0):
+    if i in s2 and i not in res and i!=' ':
+        res+=i
+res=list(res)
+res.sort()
+res=''.join(res)
+if len(res)!=0:
     print(res)
 else:
     print(-1)
